823ad8169e4d4cdd3cdab5956c591d19286b64d2
NinjaKnight05 <161373638+NinjaKnight05@users.noreply.github.com>
2026-08-19 19:31:22 +0530

    binary

--- searching.py
+++ searching.py
@@ -15,5 +15,20 @@ def linear_search(arr,target):
     return -1
 arr = [27,13,45,20,32]
 print(linear_search(arr,20))
-
+ 
 # Binary Search
+def binary_search(arr,target):
+    left,right = 0, (len(arr)-1)
+    while left < right:
+        mid = (left+right)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid+1
+        else:
+            right = mid-1
+    return -1
+
+arr = [10,20,30,40,50,60,70,80,90,100]
+print(binary_search(arr,100))
+
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def twoPointer_reverse():
-
-
-# arr = [1,2,3,4,5]
-# print(twoPointer_reverse(arr))
